2330f94cc5b57f0f8f747738cd6288ed1e002be9
Diogo Nunes <diogoabnunes@gmail.com>
2020-05-12 00:26:55 +0100

    organizing

--- proj/main.js
+++ proj/main.js
@@ -4,7 +4,7 @@ serialInclude=function(a){var b=console,c=serialInclude.l;if(a.length>0)c.splice
 
 serialInclude(['../lib/CGF.js','MyScene.js', 'MyInterface.js', 
     'MySphere.js', 
-    'MyCilinder.js', 
+    'vehicle/MyCilinder.js', 
     'MyCubeMap.js', 
     'vehicle/MyVehicle.js',
     'vehicle/MyVehicleModel.js',
